8ff030f18b5177673727bdc9c0e1893e72fa40a1
leoGeraldino <leo.nardo_geraldino@hotmail.com>
2024-07-15 22:33:57 -0300

    This file is an auxiliar file to pick the mouse position with pyautogui

---
+++ mouse_position.py
@@ -0,0 +1,9 @@
+import time, pyautogui #import time and pyautogui libraries
+
+time.sleep(5)   #the algorithm will wait for 5 seconds, enough time to user click on the right field
+
+print(pyautogui.position()) #this command prints the mouse's position 
+
+#(x=425, y=396) to click on field e-mail on login page
+
+#(x=414, y=285) to click on Código de Produto's field
